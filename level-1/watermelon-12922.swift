f987c7757b7d465bf3412a560221bc7aa619f463
cmindy <minjidev@naver.com>
2019-07-10 18:04:40 +0900

    Add watermelon.swift to level-1

--- level-1/watermelon-12922.swift
+++ level-1/watermelon-12922.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+// https://programmers.co.kr/learn/courses/30/lessons/12922
+
+func solution(_ n: Int) -> String {
+    var watermelons = Array(repeating: "수박", count: n/2)
+    if n % 2 != 0 {
+        watermelons.append("수")
+    }
+    return watermelons.joined()
+}
