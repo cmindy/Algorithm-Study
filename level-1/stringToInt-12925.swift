7e544f173373e0659f5a895fb6947d83f530e56d
cmindy <minjidev@naver.com>
2019-07-17 17:00:54 +0900

    문자열을 정수로 바꾸기 추가

---
+++ level-1/stringToInt-12925.swift
@@ -0,0 +1,14 @@
+//
+//  stringToInt-12925.swift
+//  
+//
+//  Created by CHOMINJI on 11/07/2019.
+//
+
+import Foundation
+
+https://programmers.co.kr/learn/courses/30/lessons/12925?language=swift
+
+func solution(_ s:String) -> Int {
+    return (Int(s) ?? 0)
+}
